1fde4603706336075be3f5660786b07ea33cdbd6
Chris Catacata <chris.catacata@frogs42.com>
2022-11-03 19:53:12 +0100

    added authentication

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import json
+import yaml
 import pandas as pd
 import streamlit as st
+import streamlit_authenticator as stauth
+from yaml import SafeLoader
+
 from turn import TurnDataset
 from constants import TURNS
 
@@ -8,13 +12,6 @@ from constants import TURNS
 
 # Set the page layout to wide
 st.set_page_config(layout="wide", page_title="Turn results")
-
-with st.sidebar:
-    st.markdown('# :tada: Turn result dashboard :tada:')
-    turn = st.selectbox('Select a turn', TURNS)
-    st.markdown('<br/><br/>', unsafe_allow_html=True)
-
-
 
 
 @st.cache(allow_output_mutation=True)
@@ -40,8 +37,51 @@ def load_data(turn: str) -> dict:
         'research_findings': research_findings
     }}
 
+def create_block(title: str, column_selection: list = None, data: pd.DataFrame = None, chart = None, markdown: str = None):
+    """Creates a block with the given title and text."""
 
-def main():
+    if title:
+        st.markdown(f'## {title}')
+        if data is not None:
+            if column_selection is not None:
+                st.dataframe(data[column_selection], use_container_width=True)
+            else:
+                st.dataframe(data, use_container_width=True)
+        if chart is not None:
+            st.plotly_chart(chart, use_container_width=True)
+        if markdown is not None:
+            st.markdown(markdown, unsafe_allow_html=True)
+        st.markdown('<br />', unsafe_allow_html=True)
+
+with open('config.yaml', 'r') as f:
+    config = yaml.load(f, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+with st.sidebar:
+    st.markdown('# :tada: Turn result dashboard :tada:')
+    turn = st.selectbox('Select a turn', TURNS)
+    st.markdown('<br/><br/>', unsafe_allow_html=True)
+
+
+
+
+
+if authentication_status:
+
+    st.write(f'Welcome *{name}*!')
+    with st.sidebar:
+        st.markdown('# :tada: Turn result dashboard :tada:')
+        turn = st.selectbox('Select a turn', TURNS)
+        st.markdown('<br/><br/>', unsafe_allow_html=True)
     
     data = load_data(turn)
 
@@ -51,9 +91,14 @@ def main():
     models = data[turn]['models']
     research_objectives = data[turn]['research_objectives']
     research_findings = data[turn]['research_findings']
-    
-    # Load dataset
+
+    # Load dataset and charts
     turn_dataset = TurnDataset(dataset, turn, models, turn_type)
+    n_texts, n_users, n_ratings = turn_dataset.get_general_info()
+    listvalue_df = turn_dataset.df[turn_dataset.df['model'].isin(turn_dataset.models.values())]
+    bar_chart = turn_dataset.plotly_bar_chart()
+    box_plot = turn_dataset.plotly_box_plot()
+    significance_table = turn_dataset.generate_significance_table()
 
     st.title(f'{turn}')
     st.markdown('<br />', unsafe_allow_html=True)
@@ -91,90 +136,39 @@ def main():
     
 
     # Research findings section
-    st.markdown('## Research findings')
-    st.markdown(research_findings, unsafe_allow_html=True)
+    create_block(title='Research findings', markdown=research_findings)
 
-    # Type-dependent graphs
+    # Mean summary section
+    mean_summary = turn_dataset.eval_frame().T
+    mean_summary = mean_summary.rename(columns=models)
+    column_selection = st.multiselect('Select models', mean_summary.columns)
+    if column_selection:
+        st.dataframe(mean_summary[column_selection].style.highlight_max(axis=1, color='#6493CC').format(precision=2), use_container_width=True)
+    else:
+        st.dataframe(mean_summary.style.highlight_max(axis=1, color='#6493CC').format(precision=2), use_container_width=True)
+    st.markdown('<br/>', unsafe_allow_html=True)
+
+    # Bar chart section
+    create_block(title='Bar chart', chart=bar_chart)
+
+    # Box plot section
+    with st.expander('Show box plots'):
+        create_block(title='Box plots', chart=box_plot)
+            
+    # Significance table section
+    create_block(title='Significance table', markdown=significance_table)
+
     if turn_type == 'non-fiction':
-
-        # Mean summary section
-        mean_summary = turn_dataset.eval_frame().T
-        mean_summary = mean_summary.rename(columns=models)
-        st.markdown('## Mean summary')
-        column_selection = st.multiselect('Select models', mean_summary.columns)
-        if column_selection:
-            st.dataframe(mean_summary[column_selection].style.highlight_max(axis=1, color='#6493CC').format(precision=2), use_container_width=True)
-        else:
-            st.dataframe(mean_summary.style.highlight_max(axis=1, color='#6493CC').format(precision=2), use_container_width=True)
-
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-        # Bar chart section
-        st.markdown('## Bar chart')
-        bar_chart = turn_dataset.plotly_bar_chart()
-        st.plotly_chart(bar_chart, use_container_width=True)
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-        # Box plots section
-        st.markdown('## Box plots')
-        with st.expander('See charts'):
-            st.plotly_chart(turn_dataset.plotly_box_plot(), use_container_width=True)
-        
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-        # Significance table section
-        st.markdown('## Significance table')
-        significance_table = turn_dataset.generate_significance_table()
-        st.markdown(significance_table, unsafe_allow_html=True)
-        st.markdown('<br/>', unsafe_allow_html=True)
-
         # Fact check section
         st.markdown('## Explicit and implicit fact checks')
         col10, col11 = st.columns(2)
         with col10:
             st.plotly_chart(turn_dataset.plotly_pie_chart('factual_correctness'), use_container_width=True)
-        
+                
         with col11:
             st.plotly_chart(turn_dataset.plotly_pie_chart('implicit_fact_check'), use_container_width=True)
-
-
-    elif turn_type == 'fiction':
-        pass
-
-    elif turn_type == 'horoscope':
-        # Mean summary section
-        mean_summary = turn_dataset.eval_frame().T
-        mean_summary = mean_summary.rename(columns=models)
-        st.markdown('## Mean summary')
-        column_selection = st.multiselect('Select models', mean_summary.columns)
-        if column_selection:
-            st.dataframe(mean_summary[column_selection].style.highlight_max(axis=1, color='#6493CC').format(precision=2), use_container_width=True)
-        else:
-            st.dataframe(mean_summary.style.highlight_max(axis=1, color='#6493CC').format(precision=2), use_container_width=True)
-
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-        # Bar chart section
-        st.markdown('## Bar chart')
-        bar_chart = turn_dataset.plotly_bar_chart()
-        st.plotly_chart(bar_chart, use_container_width=True)
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-        # Significance table section
-        st.markdown('## Significance table')
-        significance_table = turn_dataset.generate_significance_table()
-        st.markdown(significance_table, unsafe_allow_html=True)
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-        # Box plots section
-        st.markdown('## Box plots')
-        st.plotly_chart(turn_dataset.plotly_box_plot(), use_container_width=True)
-        st.markdown('<br/>', unsafe_allow_html=True)
-
-
     
+    authenticator.logout('Logout', 'sidebar')
 
-
-
-if __name__ == '__main__':
-    main()
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
